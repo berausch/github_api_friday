74b9b592559520b840e58cc1d2962c8df2116b03
Brittany Rausch <berausch247@gmail.com>
2016-08-05 13:45:23 -0700

    added more profile info and styling

--- js/gitapi-interface.js
+++ js/gitapi-interface.js
@@ -2,10 +2,20 @@ var allRepos = [];
 exports.getRepos = function(user, apiKey){
   $.get('https://api.github.com/users/' + user +'?access_token=' + apiKey).then(function(response){
     console.log(response);
+    var profileName = response.name;
+    var bio = response.bio;
     $("#outputArea").show();
     $("#outputLeft").empty().append("<img id='profilePic' src=" + response.avatar_url + ">");
-    $("#outputRight").empty().append("<h2>Username: <a href=" + response.html_url + ">" + response.login + "</a></h2>" )
-    $("#outputRight").append('<h4>followers:<a href=' + response.followers_url +'> ' + response.followers + '</a>     |     following:<a href='+ response.following_url + '> ' + response.following +'</h4>')
+    $("#outputRight").empty().append("<h2><a href=" + response.html_url + ">" + response.login + "</a></h2>" );
+    if(profileName !== null){
+      $("#outputRight").append("<h3 class='bold'>" + profileName + "</h3><br>" );
+    } else {
+      $("#outputRight").addClass("space");
+    }
+    if(bio !== null){
+      $("#outputRight").append("<h4><span class='bold'>Bio: </span>" + bio + "</h4><br>" );
+    }
+    $("#outputRight").append('<h4>followers:<a href=' + response.followers_url +'> ' + response.followers + '</a>     |     following:<a href='+ response.following_url + '> ' + response.following +'</h4>');
     $("#output").empty();
     $.get(response.repos_url + '?access_token=' + apiKey).then(function(response2){
       console.log(response2);
