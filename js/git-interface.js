b70eeb28efc5702daa7afb9a151f64a2effdb7ca
Brittany Rausch <berausch247@gmail.com>
2016-08-05 10:08:37 -0700

    Add the api key functionality

--- js/git-interface.js
+++ js/git-interface.js
@@ -3,5 +3,14 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
 
+$("#gitUserForm").submit(function(event){
+  event.preventDefault();
+  console.log(apiKey);
+    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+  });
 
+});
 });
